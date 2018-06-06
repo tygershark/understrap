8371e54b30c6719fbcec5b505121c0b232bb4ce1
Tristan Gemus <tgemus@tygershark.com>
2018-06-06 13:23:01 -0400

    admin css

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -27,6 +27,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 	 * Load theme's JavaScript and CSS sources.
 	 */
 	function understrap_scripts() {
+		$the_theme = wp_get_theme();
 
 		wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/'. asset_path('css/theme.min.css'), array(), null);
 
@@ -35,6 +36,10 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/' . asset_path('js/theme.min.js'), array(), null, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
+		}
+
+		if ( is_user_logged_in() ) {
+			wp_enqueue_style( 'ts-admin', get_stylesheet_directory_uri() . '/inc/admin.css', [], $the_theme->get( 'Version' ) );
 		}
 	}
 } // endif function_exists( 'understrap_scripts' ).
